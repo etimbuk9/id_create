67adfc810ce5bac8d6d0aa901b410f4cdde68a9e
etimbuk9 <etimbuk9@yahoo.co.uk>
2023-02-20 17:48:34 +0100

    redo

--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Programme, Level, Setting
-
-# Register your models here.
-admin.site.register(Setting)
-admin.site.register(Programme)
-admin.site.register(Level)
