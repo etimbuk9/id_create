b860af910f86d2723e0d7c3d331995793052ebc4
etimbuk9 <etimbuk9@yahoo.co.uk>
2023-02-22 00:08:09 +0100

    debug to true

--- id_create/settings.py
+++ id_create/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
